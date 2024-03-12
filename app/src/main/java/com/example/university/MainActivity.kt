8f98acf0ded55ae73005816b03b290412c9ab7cd
f4est <161470272+f4est@users.noreply.github.com>
2024-03-12 17:15:00 +0500

    Сделал херню

--- app/src/main/java/com/example/university/MainActivity.kt
+++ app/src/main/java/com/example/university/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.university
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MainList::class.java)
+        val timer = Timer()
+        val task = object : TimerTask() {
+            override fun run() {
+                startActivity(intent)
+                timer.cancel()
+            }
+        }
+        timer.scheduleAtFixedRate(task, 3000, 10)
+
     }
 }
